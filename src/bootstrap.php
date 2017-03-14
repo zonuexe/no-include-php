66a97c5fd7339a8c8618745f1ea74362f7c16a08
USAMI Kenta <tadsan@zonu.me>
2017-03-15 02:59:08 +0900

    include_once やめて spl_autoload_register() にするよ

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,4 +7,19 @@
  * @license   WTFPL
  */
 
-include_once __DIR__ . '/User.php';
+namespace ZonuProject;
+
+spl_autoload_register(function ($class_name) {
+    // 名前空間の先頭が一致しなければこのプロジェクトのクラスではない
+    if (strpos($class_name, 'ZonuProject\\') !== 0) {
+        return false;
+    }
+
+    // 名前空間の \ をディレクトリの / に変換する
+    $class_file = strtr(ltrim($class_name, 'ZonuProject\\'), ['\\' => '/']);
+    $path = __DIR__ . "/{$class_file}.php";
+
+    if (file_exists($path)) {
+        require_once $path;
+    }
+});
