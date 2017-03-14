e13b9e267f7521bc58c47968636c0b78c82104be
USAMI Kenta <tadsan@zonu.me>
2017-03-15 04:50:22 +0900

    whoopsをコメントアウトして追加

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -10,3 +10,7 @@
 namespace ZonuProject;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+// $whoops = new \Whoops\Run;
+// $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+// $whoops->register();
