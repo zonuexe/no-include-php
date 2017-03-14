d25bd3f720b6d7922e113517bba173da6d581128
USAMI Kenta <tadsan@zonu.me>
2017-03-15 03:43:28 +0900

    独自のクラスローダーをやめてComposerにします

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,17 +9,4 @@
 
 namespace ZonuProject;
 
-spl_autoload_register(function ($class_name) {
-    // 名前空間の先頭が一致しなければこのプロジェクトのクラスではない
-    if (strpos($class_name, 'ZonuProject\\') !== 0) {
-        return false;
-    }
-
-    // 名前空間の \ をディレクトリの / に変換する
-    $class_file = strtr(ltrim($class_name, 'ZonuProject\\'), ['\\' => '/']);
-    $path = __DIR__ . "/{$class_file}.php";
-
-    if (file_exists($path)) {
-        require_once $path;
-    }
-});
+require_once __DIR__ . '/../vendor/autoload.php';
